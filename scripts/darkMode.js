4304426d8996af960bce95001ab99c40bf8ad8ef
jcbhmphrs <jhumphreys62782@gmail.com>
2023-01-11 10:21:43 -0700

    responsive menu close icon added

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -3,18 +3,18 @@ let lightMode = true;
 const bodyElement = document.querySelector('body');
 const cards = document.querySelectorAll('.card');
 const darkText = document.querySelectorAll('a, p, h2');
-const menuItems = document.querySelectorAll('.menuListItem')
+const menu = document.querySelector('#theMenu')
 
 
 darkModeButton.addEventListener("click", () => {
 	if (lightMode) {
-		menuItems.forEach((item) => item.classList.toggle('darkMenu'));
+		menu.classList.toggle('darkMenu');
 		bodyElement.classList.toggle('darkBody');
         darkText.forEach((item) => item.classList.toggle("darkChar"));
         cards.forEach((item) => item.classList.toggle("darkCard"));
 		lightMode = false;
 	} else if (!lightMode) {
-		menuItems.forEach((item) => item.classList.toggle('darkMenu'));
+		menu.classList.toggle('darkMenu');
 		bodyElement.classList.toggle('darkBody');
         darkText.forEach((item) => item.classList.toggle("darkChar"));
         cards.forEach((item) => item.classList.toggle("darkCard"));
