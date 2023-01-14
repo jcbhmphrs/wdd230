e410d7e09b573a8d2c4ed81adc5e3bbd8c761238
jcbhmphrs <jhumphreys62782@gmail.com>
2023-01-14 13:12:22 -0700

    added js resp. menu and dynamic footer.

---
+++ chamber/scripts/dynamic-menu.js
@@ -0,0 +1,16 @@
+const menubutton = document.querySelector("#hamburger");
+const menuitems = document.querySelectorAll(".menu-item");
+var closeMenu = true;
+
+menubutton.addEventListener("click", () => {
+	if (closeMenu) {
+		menubutton.src = 'images/close-menu-icon.png';
+		closeMenu = false;
+	} else {
+		menubutton.src = 'images/menu-icon.png';
+		closeMenu = true;
+	}
+	menuitems.forEach((item) => item.classList.toggle("open"));
+	menubutton.classList.toggle("close");
+});
+
