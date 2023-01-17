0edd98af6ea0ac41da6fef0eef5b6932e46f2755
jcbhmphrs <jhumphreys62782@gmail.com>
2023-01-17 14:39:10 -0700

    local storage and lazy loading assignments added

---
+++ week03/scripts/lazyload.js
@@ -0,0 +1,3 @@
+document.getElementById('lastModified').innerHTML = `Last Modified ${document.lastModified}`;
+const date = new Date();
+document.getElementById('copyWrite').innerHTML = `&copy ${date.getFullYear()} Jacob Humphreys, Utah, USA`;
